60ed8f9e0e4a87d2b3d7fc73d6f8f2497b3d2d6b
Alina2Voron <maya222mirlinys@gmail.com>
2025-10-17 23:29:28 +0200

    Add files via upload

---
+++ gold_rush/src/main/java/edu/io/Main.java
@@ -0,0 +1,23 @@
+package edu.io;
+
+public class Main {
+
+    public static final String GOLD_TOKEN_LABEL = "\uD83D\uDCB0";
+    public static final String PLAYER_TOKEN_LABEL = "\uC6C3";
+
+    public static void main(String[] args) {
+        System.out.println("Gold Rush");
+
+                Board board = new Board();
+
+                Token gold = new Token(GOLD_TOKEN_LABEL);
+                Token player = new Token(PLAYER_TOKEN_LABEL);
+
+                board.clean();
+
+                board.placeToken(5, 4, gold);
+                board.placeToken(2, 3, player);
+
+                board.display();
+            }
+        }
