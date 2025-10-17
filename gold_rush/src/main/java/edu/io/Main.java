2a5822551f8a7abae7c6530e16cc8c9901271010
Alina2Voron <maya222mirlinys@gmail.com>
2025-10-17 21:14:06 +0200

    Update Main.java

--- gold_rush/src/main/java/edu/io/Main.java
+++ gold_rush/src/main/java/edu/io/Main.java
@@ -1,13 +1,17 @@
 package edu.io;
 
 public class Main {
+
+    public static final String GOLD_TOKEN_LABEL = "\uD83D\uDCB0"; 
+    public static final String PLAYER_TOKEN_LABEL = "\uC6C3";  
+
     public static void main(String[] args) {
         System.out.println("Gold Rush");
 
         Board board = new Board();
 
-        Token gold = new Token(Board.GOLD_TOKEN_LABEL);
-        Token player = new Token(Board.PLAYER_TOKEN_LABEL);
+        Token gold = new Token(GOLD_TOKEN_LABEL);
+        Token player = new Token(PLAYER_TOKEN_LABEL);
 
         board.placeToken(5, 4, gold);
         board.placeToken(2, 3, player);
