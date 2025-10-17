6efb1fa5cb640ab39c813067aad461e88c7a872a
Alina2Voron <maya222mirlinys@gmail.com>
2025-10-17 21:13:05 +0200

    Update Board.java

--- gold_rush/src/main/java/edu/io/Board.java
+++ gold_rush/src/main/java/edu/io/Board.java
@@ -5,8 +5,6 @@ public class Board {
     private Token[][] grid;
 
     public static final String EMPTY_TOKEN_LABEL = "\u30FB";
-    public static final String GOLD_TOKEN_LABEL = "\uD83D\uDCB0";
-    public static final String PLAYER_TOKEN_LABEL = "\uC6C3";
 
     public Board() {
         grid = new Token[size][size];
